cafc127744f49e7f16c50d29c290f75005901622
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2021-07-25 18:43:34 +0000

    vue Template-project oluşturuldu.

---
+++ vue/template-project/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-secondary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">OpenAI</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav"  >
+          <slot></slot>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",  
+};
+</script>
