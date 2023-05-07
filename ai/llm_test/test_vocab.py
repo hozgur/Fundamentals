a89013aa3d5e49ad46d7156d3994be61a9403572
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2023-05-07 23:33:45 +0300

    vocab hazirlamaya devam

--- ai/llm_test/test_vocab.py
+++ ai/llm_test/test_vocab.py
@@ -1,25 +1,28 @@
-from vocab import remove_punctuation, create_words, create_vocabulary, read_text
+from vocabulary import remove_punctuation, create_vocabulary, read_text
 
 def test_remove_punctuation():
-    sample_word = "!!(hello)!:"
-    expected = ["!", "!", "(", "hello", ")", "!", ":"]
+    sample_word = "!!(ben-im’den)!:"
+    expected = ["!", "!", "(", "ben","-", "im","’", "den", ")", "!", ":"]
     actual = remove_punctuation(sample_word)
     assert actual == expected
 
-def test_create_words():
-    sample_text = "Hello, how are you?"
-    expected = ["hello", ",", "how", "are", "you", "?"]
-    actual = create_words(sample_text.lower())
+    sample_word = "--"
+    expected = ["-", "-"]
+    actual = remove_punctuation(sample_word)
     assert actual == expected
+
 
 def test_create_vocabulary():
     sample_text = "Hello, how are you?"
-    expected = ({"hello": 1, ",": 2, "how": 3, "are": 4, "you": 5, "?": 6}, {1: "hello", 2: ",", 3: "how", 4: "are", 5: "you", 6: "?"})
+    tokens = ["hello", ",", "how", "are", "you", "?"]
+    expected = (
+                {",": 1, "?": 2, "are": 3, "hello": 4, "how": 5, "you": 6},
+                {1: ",", 2: "?", 3: "are", 4: "hello", 5: "how", 6: "you"})
     actual = create_vocabulary(sample_text)
     assert actual == expected
 
 
 if __name__ == "__main__":
     test_remove_punctuation()
-    test_create_words()
+    test_create_vocabulary()
     print("All tests passed!")
--- ai/llm_test/vocabulary.py
+++ ai/llm_test/vocabulary.py
@@ -1,15 +1,36 @@
-#vocab.py
+#vocabulary.py
 import string
 punctuation = string.punctuation + '”' + '“' + '—' + '‘' + '’' + '…' + '«' + '»' + '–'
+vocab = None
+tokens = None
+
+def remove_middle(word,symbol):
+    assert (symbol in word)
+    pre_tokens = []
+    post_tokens = []
+    words = word.split(symbol)
+    pre_tokens.extend(remove_punctuation(words[0]))
+    post_tokens.extend(remove_punctuation(words[1]))
+    return pre_tokens  + [symbol] +  post_tokens    
+
 def remove_punctuation(word):
     pre_tokens = []
     post_tokens = []
+    #find apostrophe and split word
+    if "’" in word:
+        return remove_middle(word,"’")
+    #find hyphen and split word
+    if len(word) > 3 and "–" in word:
+        return remove_middle(word,"–")
+
+    #pre_punctions
     if word[0] in punctuation:
         while word[0] in punctuation:
             pre_tokens.append(word[0])
             word = word[1:]
             if len(word) == 0:
                 break    
+    #post_punctions
     if word[-1] in punctuation:
         while word[-1] in punctuation:
             post_tokens.append(word[-1])
@@ -24,22 +45,21 @@ def read_text(text_file):
     f.close()
     return text
 
-def create_words(text):
-    words = []
-    for word in text.split():
-        words.extend(remove_punctuation(word))
-    return words
+def create_tokens(words):
+    tokens = []
+    for word in words:
+        tokens.extend(remove_punctuation(word))
+    return tokens
 
 
-def create_vocabulary(text_file):
-    text = read_text(text_file)
-    words = create_words(text)
-    vocab = set(words)
+def create_vocabulary(text):
+    global vocab
+    global tokens
+    words = text.lower().split()
+    tokens = create_tokens(words)
+    vocab = set(tokens)
     # convert words to integers
     vocab = sorted(vocab)
     vocab_to_int = {word: ii for ii, word in enumerate(vocab, 1)}
     int_to_vocab = {ii: word for ii, word in enumerate(vocab, 1)}
     return vocab_to_int, int_to_vocab
-    
-    
-    
